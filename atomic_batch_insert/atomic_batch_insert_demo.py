dd517e4b11600311311442132d3bb07231abaae6
xtcyclist <7731943+xtcyclist@users.noreply.github.com>
2022-08-30 11:20:33 +0800

    Add retry

--- atomic_batch_insert/atomic_batch_insert_demo.py
+++ atomic_batch_insert/atomic_batch_insert_demo.py
@@ -4,9 +4,16 @@ from nebula3.gclient.net import ConnectionPool
 from nebula3.Config import Config
 import csv
 import sys
+import logging
 
+# Query template for todo queries
 insertVertexTemplate = "INSERT VERTEX player(name, age) VALUES \"%s\":(\"%s\", %s)"
+# Query template for undo queries that delete the inserted vertices
 rollbackTemplate = "DELETE VERTEX \"%s\""
+# Retry each query execution for the following times
+retryTimes = 10
+
+logging.basicConfig(level=logging.INFO)
 
 def readCSV(filename):
   data = []
@@ -14,9 +21,13 @@ def readCSV(filename):
     csvFile = csv.reader(f, delimiter = ',')
     for line in csvFile:
       data.append([line[0], line[1]])
-  print(data)
   return data
 
+# Generate two lists of queries: todo and undo.
+# (1) Append each query waiting to be executed into the todo list.
+# (2) For each such query, prepare an counterpart that undos its changes in the database.
+#     Append this undo counterpart into the undo list.
+# (3) The undo counterpart of the i-th todo query is the i-th query in the undo list.
 def genBatch(data):
   todo = []
   undo = []
@@ -25,35 +36,59 @@ def genBatch(data):
     rollback = rollbackTemplate % (player[0])
     todo.append(insert)
     undo.append(rollback)
+  # Ingest some errors for testing:
   # undo[0] = insertVertexTemplate
-  # todo[1] = insertVertexTemplate
+  # todo[4] = insertVertexTemplate
   return todo, undo
 
+# Execute the given query multiple times.
+# Return a None as the result, if all attempted executions failed.
+# The error code and message of the last attempt is logged.
+def exeQueryWithRetries(query, session):
+  result = session.execute(query)
+  if result.is_succeeded():
+    return result
+  i = 0
+  while i < retryTimes:
+    result = session.execute(query)
+    if not result.is_succeeded():
+      i = i + 1
+    else:
+      break
+  if i == retryTimes:
+    logging.error("Error %s (%d), while executing query %s." % (result.error_msg(), result.error_code(), query))
+    return None
+  elif result.is_succeeded():
+    return result
+  else:
+    raise Exception("Failed at trying to execute query %s." % (query))
+
+# Execute queries in a batch.
 def exeBatch(space, batch, session):
   counter = 0
   session.execute("use " + space)
   for query in batch:
-    result = session.execute(query)
-    if result.is_succeeded():
-      counter = counter + 1
-    else:
+    result = exeQueryWithRetries(query, session)
+    if result == None:
       return counter
   return counter
 
+# Rollback the batch execution by executing the undo counterparts of all successfully executed queries.
 def rollback(undo, progress, session):
   count = 0
   while (count < progress):
-    result = session.execute(undo[count])
-    if result.is_succeeded():
-      count = count + 1
-    else:
-      raise Exception("Rollback failed while executing the %d-th undo statement \"%s\"." % (count, undo[count]))
+    result = exeQueryWithRetries(undo[count], session)
+    if result == None:
+      logging.error("Rollback failed while executing the %d-th undo statement \"%s\"." % (count, undo[count]))
+      return False
   if count == progress:
     return True
   else:
     return False
 
+# Main entry
 if __name__ == "__main__":
+  # Load data to fill query templates from a csv
   csvfile = sys.argv[1]
   data = readCSV(csvfile)
   todo, undo = genBatch(data)
@@ -68,7 +103,7 @@ if __name__ == "__main__":
         if rollback(undo, progress, session) == False:
           raise Exception("Rollback failed.")
         else:
-          print ("Bacth insert failed, with all inserted vertices rolled back.")
+          logging.info("Bacth insert failed, with all inserted vertices rolled back.")
       else:
-        print ("Batch insert succeeded.")
+        logging.info("Batch insert succeeded.")
   conn.close()
